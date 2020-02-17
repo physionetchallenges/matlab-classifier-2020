71e6d3acdc9076c2e4d36a0f99690f31e6d7dcd8
Erick Andres Perez Alday <36280580+perezald@users.noreply.github.com>
2020-02-17 11:35:50 -0500

    Add files via upload

--- driver.m
+++ driver.m
@@ -2,10 +2,10 @@ function driver(input_directory, output_directory)
 
     
 	% Find files.
-    files = {};
+    input_files = {};
     for f = dir(input_directory)'
         if exist(fullfile(input_directory, f.name), 'file') == 2 && f.name(1) ~= '.' && all(f.name(end - 2 : end) == 'mat')
-            files{end + 1} = f.name;
+            input_files{end + 1} = f.name;
         end
     end
 
@@ -13,23 +13,28 @@ function driver(input_directory, output_directory)
         mkdir(output_directory)
     end
 
+    
+    % read number of unique classes
+    classes = get_classes(input_directory,input_files);
+
+
     % Load model.
     disp('Loading 12ECG model...')
     model = load_12ECG_model();
 
     % Iterate over files.
     disp('Predicting 12ECG labels...')
-    num_files = length(files);
+    num_files = length(input_files);
     for i = 1:num_files
         disp(['    ', num2str(i), '/', num2str(num_files), '...'])
 
         % Load data.
-        file_tmp=strsplit(files{i},'.');
-        input_file = fullfile(input_directory, file_tmp{1});
-        [data,hea_data] = load_challenge_data(input_file);
-        [classes,current_score,current_label] = get_12ECG_score(data,hea_data,model);
+        file_tmp=strsplit(input_files{i},'.');
+        tmp_input_file = fullfile(input_directory, file_tmp{1});
+        [data,header_data] = load_challenge_data(tmp_input_file);
+        [current_score,current_label] = get_12ECG_score(data,header_data,classes,model);
 
-        save_challenge_predictions(output_directory,files{i}, current_score, current_label,classes);
+        save_challenge_predictions(output_directory,file_tmp{1}, current_score, current_label,classes);
 	
     end
 
@@ -63,20 +68,55 @@ function [data,tlines] = load_challenge_data(filename)
 
 end
 
-function save_challenge_predictions(output_directory,file, scores, labels,classes)
+% find unique number of classes
+function classes = get_classes(input_directory,files)
+	
+	classes={};
+	num_files = length(files);
+	k=1;
+    	for i = 1:num_files
+		g = strrep(files{i},'.mat','.hea');
+		input_file = fullfile(input_directory, g);
+	        fid=fopen(input_file);
+	        tline = fgetl(fid);
+        	tlines = cell(0,1);
 
-	filename = strsplit(file,'.');
-	output_file = ([output_directory filesep filename{1} '.csv']);
+		while ischar(tline)
+        	    tlines{end+1,1} = tline;
+	            tline = fgetl(fid);
+			if startsWith(tline,'#Dx')
+				tmp = strsplit(tline,': ');
+				tmp_c = strsplit(tmp{2},',');
+				for j=1:length(tmp_c)
+		                	idx2 = find(strcmp(classes,tmp_c{j}));
+		                	if isempty(idx2)
+                	        		classes{k}=tmp_c{j};
+                        			k=k+1;
+                			end
+				end
+			break
+        		end
+		end
+
+        	fclose(fid);
+
+	end
+end
+
+
+
+function save_challenge_predictions(output_directory,recording, scores, labels,classes)
+
+	output_file = ([output_directory filesep recording '.csv']);
 
 	commaHeader = [classes;repmat({','},1,numel(classes))]; %insert commaas
 	commaHeader = commaHeader(:)';
 	textHeader = cell2mat(commaHeader); %cHeader in text with commas
 	%write header to file
-	fid = fopen(output_file,'w'); 
-	fprintf(fid,'#%s\n',filename{1});
+	fid = fopen(output_file,'w');
+	fprintf(fid,'#%s\n',recording);
 	fprintf(fid,'%s\n',textHeader);
 	fclose(fid);
-
 
 	%write data to end of file
 	dlmwrite(output_file,labels,'delimiter',',','-append');
--- get_12ECG_score.m
+++ get_12ECG_score.m
@@ -1,17 +1,16 @@
-function [classes,score, label] = get_12ECG_score(data,hea_data, model)
+function [score, label] = get_12ECG_score(data,header_data,classes, model)
 
-    classes = {'Normal', 'AF', 'I-AVB', 'LBBB', 'RBBB', 'PAC','PVC','STD','STE'};
-    num_of_classes = length(classes);
+    num_classes = length(classes);
 
-    label = zeros([1,num_of_classes]);
-    score = ones([1,num_of_classes]);
+    label = zeros([1,num_classes]);
+    score = ones([1,num_classes]);
     
     % Use your classifier here to obtain a label and score for each class.
-    tmp_features = get_12ECG_features(data,hea_data);
-    [tmp_label,tmp_score] = predict(model,tmp_features);
+    features = get_12ECG_features(data,header_data);
+    [tmp_label,tmp_score] = predict(model,features);
 
     label(tmp_label)=1;
-    score=score*.1;%mp_score;
+    score=tmp_score;
 
 end
 
