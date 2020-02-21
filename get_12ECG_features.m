afb70c74738be8adf0cd870ba8dc99712b8c7556
Erick Andres Perez Alday <36280580+perezald@users.noreply.github.com>
2020-02-21 18:01:32 -0500

    Update get_12ECG_features.m

--- get_12ECG_features.m
+++ get_12ECG_features.m
@@ -1,3 +1,25 @@
+% This function calculates RR intervals and SQI values using 
+% the HRV toolbox (https://github.com/cliffordlab/PhysioNet-Cardiovascular-Signal-Toolbox.git)
+% more information can be found: 
+% Vest A, Da Poian G, Li Q, Liu C, Nemati S, Shah A, Clifford GD, 
+% "An Open Source Benchmarked Toolbox for Cardiovascular Waveform and Interval Analysis", 
+%  Physiological measurement 39, no. 10 (2018): 105004. DOI:10.5281/zenodo.1243111; 2018. 
+
+% Then, it finds the fiducial points using ECGkit (https://github.com/marianux/ecg-kit.git)
+% More information can be found:
+% Demski AJ, Llamedo Soria M. ecg-kit a Matlab Toolbox for Cardiovascular Signal Processing. 
+% Journal of Open Research Software. 2016;4(1):e8. DOI: http://doi.org/10.5334/jors.86
+
+% It creates a temporal beat alignment using (https://github.com/Tereshchenkolab/Origin.git)
+% and then extract the GEH parameters using (https://github.com/Tereshchenkolab/Global-Electrical-Heterogeneity.git)
+% More information can be found:
+% Erick Perez-Alday,et al; Importance of the Heart Vector Origin Point Definition for an ECG analysis: 
+% The Atherosclerosis Risk in Communities (ARIC) study. Comp Biol Med, Volume 104, January 2019, 
+% pages 127-138. https://doi.org/10.1016/j.compbiomed.2018.11.013
+% Waks JW, et al. Global Electric Heterogeneity Risk Score for Prediction of Sudden Cardiac Death in the General Population: 
+% The Atherosclerosis Risk in Communities (ARIC) and Cardiovascular Health (CHS) Studies. Circulation. 2016;133:2222-2234.
+%
+
 function features = get_12ECG_features(data, header_data)
 
        % addfunction path needed
