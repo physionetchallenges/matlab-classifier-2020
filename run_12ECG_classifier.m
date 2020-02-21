8388f37c97a4d768d5d4db0eb7b89b6767496bbc
Erick Andres Perez Alday <36280580+perezald@users.noreply.github.com>
2020-02-21 18:06:23 -0500

    Add files via upload

---
+++ run_12ECG_classifier.m
@@ -0,0 +1,20 @@
+function [score, label] = run_12ECG_classifier(data,header_data,classes, model)
+
+
+    num_classes = length(classes);
+
+    label = zeros([1,num_classes]);
+    score = ones([1,num_classes]);
+    
+    % Use your classifier here to obtain a label and score for each class.
+    features = get_12ECG_features(data,header_data);
+    [tmp_label,tmp_score] = predict(model,features);
+
+
+    label(tmp_label)=1;
+    score=tmp_score;
+
+end
+
+
+
